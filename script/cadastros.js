9b0b82872b258e9ee67379d31b27aba706243018
vanagila <vanagilakedna@gmail.com>
2025-06-19 17:14:47 -0300

    feat: cadastro de enfermeiros

--- script/cadastros.js
+++ script/cadastros.js
@@ -1,34 +1,51 @@
-import { medicos } from "./dados.js";
-const formCadastro = document.querySelector("#cadastro-form");
+import { medicos, enfermeiros } from "./dados.js";
+const form = document.querySelector(".cadastro-form");
+const tipo = form.dataset.tipo;
+console.log(tipo)
 
-formCadastro.addEventListener("submit", (ev) => {
-    ev.preventDefault();
+switch (tipo) {
+    case "medico":
+    case "enfermeiro":
+        form.addEventListener("submit", (ev) => {
+            ev.preventDefault();
+        
+            const nome = document.getElementById("nome").value;
+            const dtNascimento = document.getElementById("dtNascimento").value;
+            const endereco = document.getElementById("endereco").value;
+            const bairro = document.getElementById("bairro").value;
+            const nacionalidade = document.getElementById("nacionalidade").value;
+            const telefone = document.getElementById("telefone").value;
+            const genero = document.getElementById("genero").value;
+            const especialidade = document.getElementById("especialidade").value;
+        
+            if (!nome || !dtNascimento || !endereco || !bairro || !nacionalidade || !telefone || !genero || !especialidade) {
+                alert("Preencha todos os campos.")
+                return
+            }
 
-    const nome = document.getElementById("nome").value;
-    const dtNascimento = document.getElementById("dtNascimento").value;
-    const endereco = document.getElementById("endereco").value;
-    const bairro = document.getElementById("bairro").value;
-    const nacionalidade = document.getElementById("nacionalidade").value;
-    const telefone = document.getElementById("telefone").value;
-    const genero = document.getElementById("genero").value;
-    const especialidade = document.getElementById("especialidade").value;
+            const novoCadastro = {
+                nome,
+                dtNascimento,
+                endereco,
+                bairro,
+                nacionalidade,
+                telefone,
+                genero,
+                especialidade
+            }
 
-    if (!nome || !dtNascimento || !endereco || !bairro || !nacionalidade || !telefone || !genero || !especialidade) {
-        alert("Preencha todos os campos.")
-        return
-    }
-
-    const doctor = {
-        nome,
-        dtNascimento,
-        endereco,
-        bairro,
-        nacionalidade,
-        telefone,
-        genero,
-        especialidade
-    }
-
-    medicos.push(doctor);
-    formCadastro.reset();
-});
+            if (tipo === "medico") {
+                medicos.push(novoCadastro);
+                console.log(medicos)
+            }
+            else {
+                enfermeiros.push(novoCadastro);
+                console.log(enfermeiros)
+            }
+            
+            form.reset();
+        });
+        break;
+    default:
+        console.log("outro tipo");
+}
